d67ecf7d57e546428ac149bfb6beb8e4f5362618
Your Name <you@example.com>
2023-07-15 14:44:09 +0200

    add change color function

--- app.js
+++ app.js
@@ -100,6 +100,7 @@ function setProgress(percent)
 }
 
 function restartTimer(duration) {
+    pauseTimer();  // Add this line
     const timerData = timers[currentTimer];
     timerData.timeLeft = undefined; 
     circle.style.strokeDashoffset = 0; 
@@ -165,4 +166,80 @@ document.querySelector('.settings__modal__header svg').addEventListener('click',
 
 document.querySelector('.settings').addEventListener('click', () => {
     document.querySelector('.settings__modal').style.visibility = 'visible';
+});
+
+document.querySelector('.settings__modal__footer__apply').addEventListener('click', () => {
+    const durationValue = document.querySelectorAll('.settings__modal__content__item input');
+    
+    const [pomodoroDuration, shortbreakDuration, longbreakDuration] = [...durationValue].map((item) => {
+        return parseInt(item.value, 10);  // Convert string to integer
+    });
+    
+    timers.pomodoro.duration = pomodoroDuration;
+    timers.shortBreak.duration = shortbreakDuration;
+    timers.longBreak.duration = longbreakDuration;
+
+    // Reset the SVG circle
+    const svgCircle = document.querySelector('.circle');
+    svgCircle.style.strokeDasharray = '';  // Adjust as needed
+    svgCircle.style.strokeDashoffset = '';  // Adjust as needed
+
+    document.querySelector('.settings__modal').style.visibility = 'hidden';
+    restartTimer(timers.pomodoro.duration);
+});
+
+// Sélectionnez le parent contenant tous les cercles
+const colorContainer = document.querySelector('.settings__modal__content__color__item');
+
+// Sélectionnez le cercle dont la couleur du trait doit être modifiée
+const svgCircle = document.querySelector('.circle');
+
+// Select the path that draws the check
+const checkPath = document.querySelector('.settings__modal__content__color__item svg path');
+
+
+// Ajoutez un gestionnaire d'événement 'click' au parent
+colorContainer.addEventListener('click', function(event) {
+
+
+        // Vérifiez si un cercle a été cliqué
+        if (event.target.tagName === 'circle' || event.target.tagName === 'CIRCLE') {
+            // Obtenez la couleur de remplissage du cercle
+            const color = event.target.getAttribute('fill');
+            const cx = event.target.getAttribute('cx');
+    
+            // Move the check to the clicked circle
+            if (cx == '20') {
+                checkPath.setAttribute('d', 'M14 20.5L17.9526 24.4526L26.4053 16');
+            } else if (cx == '76') {
+                // Added 56 to each x-coordinate
+                checkPath.setAttribute('d', 'M70 20.5L73.9526 24.4526L82.4053 16');
+            } else if (cx == '132') {
+                // Added 112 to each x-coordinate
+                checkPath.setAttribute('d', 'M126 20.5L129.9526 24.4526L138.4053 16');
+            }
+                // Vérifiez si un cercle a été cliqué
+                if (event.target.tagName === 'circle' || event.target.tagName === 'CIRCLE') {
+                    // Obtenez la couleur de remplissage du cercle
+                    const color = event.target.getAttribute('fill');
+
+                    // Utilisez la couleur de remplissage pour déterminer quelle couleur affecter à la variable --orange
+                    switch (color) {
+                        case '#F87070':
+                            document.documentElement.style.setProperty('--orange', '#F87070');  // Keep orange
+                            svgCircle.setAttribute('stroke', '#F87070');  // Change SVG circle stroke color to orange
+                            break;
+                        case '#70F3F8':
+                            document.documentElement.style.setProperty('--orange', '#70F3F8');  // Change to cyan
+                            svgCircle.setAttribute('stroke', '#70F3F8');  // Change SVG circle stroke color to cyan
+                            break;
+                        case '#D881F8':
+                            document.documentElement.style.setProperty('--orange', '#D881F8');  // Change to violet
+                            svgCircle.setAttribute('stroke', '#D881F8');  // Change SVG circle stroke color to violet
+                            break;
+                        default:
+                            console.log('Unrecognized color: ' + color);
+                    }
+                }
+            }
 });
